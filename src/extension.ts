68c456295cef61f57c22aa7baec37194a4f92840
shenshuai_dr <shen-sh@neusoft.com>
2024-03-26 17:38:45 +0800

    画rect

--- src/extension.ts
+++ src/extension.ts
@@ -296,9 +296,45 @@ const setPanelHtml = (
         <title>git code analysis</title>
       </head>
       <body>
+        ${ChartJSSrc ? `<canvas id="myGitChat" height="400" width="660"></canvas>` : ''}       
         <h3>git code analysis</h3>
         <p>The project submitted code line by <b>${userName}</b> from <b>${since}</b> to <b>${until}</b> is as follows</p>
         <div style='font-size: 18px;'>${(result as string).slice(5)}</div>
+        <script type="text/javascript">
+          let canvas = document.getElementById('myGitChat');
+          let ctx = canvas.getContext('2d');
+          
+          function draw(){
+            var x0=30,//x轴0处坐标
+              y0=360,//y轴0处坐标
+              x1=620,//x轴顶处坐标
+              y1=30,//y轴顶处坐标
+              dis=90, //柱子间距
+              barWidth=60;
+
+            //先绘制X和Y轴
+            ctx.beginPath();
+            ctx.lineWidth=1; 
+          
+            ctx.moveTo(x0,y1);//笔移动到Y轴的顶部
+            ctx.lineTo(x0,y0);//绘制Y轴
+            ctx.lineTo(x1,y0);//绘制X轴
+            ctx.stroke();
+            // ctx.fillStyle = '#3498db';
+            // ctx.fillRect(20, 20, 145, 85);
+
+            // ctx.fillStyle = '#1bbc9d';
+            // ctx.fillRect(200, 20, 145, 85);
+            let colors = ['#3498db', '#1bbc9d', '#eab5a9', '#e7ceb6', '#c6eed2', '#b0e6e3']
+            for(let i = 0; i <6; i++){
+              ctx.fillStyle = colors[i];
+              ctx.fillText((i+1)+'月份', x0 + 50 + i * dis, y0+15);
+              ctx.fillRect(x0 + 30 + i * dis, 360, barWidth, -(85+i*30));
+              ctx.fillText(85+i*30, x0 + 50 + i * dis, y0-(95+i*30));
+            }
+          }
+          draw();
+        </script>
       </body>
     </html>
   `;
